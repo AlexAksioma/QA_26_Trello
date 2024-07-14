95f4e532cf79ba03f326ea549d35a776d335b6b8
AlexAksioma <alexmedqwerty@gmail.com>
2024-07-14 19:58:59 +0300

    create pages, login tests

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -1,2 +1,34 @@
-package manager;public class ApplicationManager {
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private ChromeOptions chromeOptions;
+    public WebDriver getDriver(){
+        return driver;
+    }
+    @BeforeMethod
+    public void setup(){
+        chromeOptions = new ChromeOptions().addArguments("--lang=en");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+        //wait for script to finish execution
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        //wait for loading page
+
+    }
+    @AfterMethod
+    public void tearDown(){
+//        if(driver!=null)
+//            driver.quit();
+    }
 }
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import dto.UserDTO;
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends ApplicationManager {
+
+    UserDTO user = UserDTO.builder()
+            .email("aksiomamedved@gmail.com")
+            .password("AlexMed123!")
+            .build();
+
+    @Test
+    public void loginTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin()
+                .typeEmail(user)
+                .typePassword(user)
+        ;
+    }
+}
